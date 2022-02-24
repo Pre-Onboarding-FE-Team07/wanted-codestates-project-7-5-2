babac1afd192699f19ed1e80affb5bde4fa2d9e9
jinyongp <dev.jinyongp@gmail.com>
2022-02-24 19:29:08 +0900

    Feat: canvas 이미지 그리기

--- src/lib/ImageDragSelector.jsx
+++ src/lib/ImageDragSelector.jsx
@@ -1,3 +1,41 @@
-export default function ImageDragSelector() {
-  return <div>ImageDragSelector</div>;
+import PropTypes from 'prop-types';
+import { useEffect, useRef } from 'react';
+
+export default function ImageDragSelector({ src, width, height }) {
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = src;
+    const ctx = canvasRef.current.getContext('2d');
+    image.addEventListener('load', () => {
+      const { naturalWidth: nw, naturalHeight: nh } = image;
+      if (width && height) {
+        image.width = width;
+        image.height = height;
+      } else if (width) {
+        image.width = width;
+        image.height = (+nh / +nw) * width;
+        height = image.height;
+      } else if (height) {
+        image.height = height;
+        image.width = (+nw / +nh) * height;
+        width = image.width;
+      } else {
+        width = image.naturalWidth;
+        height = image.naturalHeight;
+      }
+      canvasRef.current.width = width;
+      canvasRef.current.height = height;
+      ctx.drawImage(image, 0, 0, width, height);
+    });
+  }, []);
+
+  return <canvas ref={canvasRef} />;
 }
+
+ImageDragSelector.propTypes = {
+  src: PropTypes.string,
+  width: PropTypes.number,
+  height: PropTypes.number,
+};
