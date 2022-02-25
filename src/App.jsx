918d1f8fd492e0b9387582ec1403d1bce716cf7f
Park, Jinyong <21158729+jinyongp@users.noreply.github.com>
2022-02-25 11:25:43 +0900

    Deploy: 배포 준비 (#11)

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import ImageDragSelector from './lib/ImageDragSelector';
-import './styles/reset.css';
-import './styles/style.css';
-
-export default function App() {
-  return <ImageDragSelector src="fashion-unsplash.jpeg" width={500} />;
-}
--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
-import { useCanvasContext } from '../ImageDragSelector';
+import { useCanvasContext } from '../ImageAreaSelector';
 
 export default function NameList({ offsetLeft, offsetTop }) {
   const { canvasList, setCanvasList } = useCanvasContext();
+
+  // console.log(
+  //   canvasList.map(({ name, ref }) => [name, ref.current?.dataset.id])
+  // );
 
   return canvasList.length ? (
     <ul
@@ -13,15 +17,18 @@ export default function NameList({ offsetLeft, offsetTop }) {
         backgroundColor: 'rgba(255,255,255,0.8)',
         padding: 8,
         borderRadius: 10,
+        listStyle: 'none',
         zIndex: 100,
       }}
     >
-      {canvasList.map(({ name, ref: canvasRef }, index) => (
+      {canvasList.map(({ name, ref }, index) => (
         <li style={{ color: 'black', margin: 2 }} key={index}>
           <button
             style={{ fontSize: 14, padding: '0px 3px', cursor: 'pointer' }}
             onClick={() =>
-              setCanvasList(canvasList.filter(({ ref }) => ref !== canvasRef))
+              setCanvasList(
+                canvasList.filter(({ ref: canvasRef }) => canvasRef !== ref)
+              )
             }
           >
             ×
